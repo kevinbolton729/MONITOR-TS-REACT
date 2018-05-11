1b76807dcf557119ae0a4582aaba389d6629a07e
kevinbolton729 <kbolton@sohu.com>
2018-05-11 19:05:10 +0800

    修改 燃气公司运营的detailTpl.tsx

--- src/routes/Operation/detailTpl.tsx
+++ src/routes/Operation/detailTpl.tsx
@@ -19,111 +19,62 @@ export default (data: any, fn: any, opts: any) => {
   const main = (
     <div key="main">
       <div className="hangTitle">
-        <p>指令详细：</p>
+        <p>系统详细：</p>
       </div>
       <div className="hangSubTitle">
         <Row>
           <Col sm={24} md={8}>
-            <p>指令类型：</p>
+            <p>IP地址/端口号：</p>
           </Col>
           <Col sm={24} md={8}>
-            <p>指令状态：</p>
+            <p>MAC地址：</p>
           </Col>
           <Col sm={24} md={8}>
-            <p>到达时间：</p>
+            <p>数据库版本：</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={24} md={8}>
+            <p>数据库时间是否与服务器时间同步：</p>
+          </Col>
+          <Col sm={24} md={8}>
+            <p>读写器/其他设备型号：</p>
+          </Col>
+          <Col sm={24} md={8}>
+            <p>DLL文件版本：</p>
           </Col>
         </Row>
       </div>
       <Divider />
       <div className="hangTitle">
-        <p>
-          {opts.tab && opts.radio
-            ? `${opts.tab}/${opts.radio}`
-            : `${opts.sortGroup[opts.modalSort]}`}：
-        </p>
+        <p>{opts.sortGroup[opts.modalSort]}：</p>
       </div>
       <div className="hangSubTitle">
         <Row>
           <Col sm={24} md={8}>
-            <p>表编号：</p>
+            <p>省份/城市：</p>
           </Col>
-          <Col span={opts.modalSort !== 'nblot' ? 8 : 16}>
+          <Col sm={24} md={8}>
             <p>燃气公司：</p>
           </Col>
-          {opts.modalSort !== 'nblot' && (
-            <Col sm={24} md={8}>
-              <p>集中器：</p>
-            </Col>
-          )}
-        </Row>
-        <Row>
           <Col sm={24} md={8}>
-            <p>价格(元)：</p>
-          </Col>
-          <Col sm={24} md={8}>
-            <p>价格类型：</p>
-          </Col>
-          <Col sm={24} md={8}>
-            <p>价格版本：</p>
+            <p>公司编号：</p>
           </Col>
         </Row>
         <Row>
           <Col sm={24} md={8}>
-            <p>价格状态：</p>
+            <p>系统名称 / 版本号：</p>
           </Col>
           <Col sm={24} md={8}>
-            <p>调价时间：</p>
+            <p>采集方式：</p>
           </Col>
           <Col sm={24} md={8}>
-            <p>有效期至：</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm={24} md={8}>
-            <p>电池状态：</p>
-          </Col>
-          <Col sm={24} md={8}>
-            <p>电池电压：</p>
-          </Col>
-          <Col sm={24} md={8}>
-            <p>时长(使用/理想)：</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm={24} md={8}>
-            <p>阀门状态：</p>
-          </Col>
-          <Col span={16}>
-            <p>指令执行后(阀门控制)：</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm={24} md={8}>
-            <p>上报状态：</p>
-          </Col>
-          <Col sm={24} md={8}>
-            <p>未上报/已上报：</p>
-          </Col>
-          <Col sm={24} md={8}>
-            <p>上报时间：</p>
-          </Col>
-        </Row>
-      </div>
-      <div className="hangTitle">
-        <p>燃气用户：</p>
-      </div>
-      <div className="hangSubTitle">
-        <Row>
-          <Col sm={24} md={8}>
-            <p>姓名：</p>
-          </Col>
-          <Col span={16}>
-            <p>卡号：</p>
+            <p>采集数据时间：</p>
           </Col>
         </Row>
         <Row>
           <Col span={24}>
-            <p>详细地址：</p>
+            <p>备注说明：</p>
           </Col>
         </Row>
       </div>
